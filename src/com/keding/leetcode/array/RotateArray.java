39c6ae6c9124b543e5cd4505e36e4b128658b036
sunkeding <970335012@qq.com>
2020-08-01 10:13:19 +0800

    旋转数组，利用三次反转实现，题目：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/

---
+++ src/com/keding/leetcode/array/RotateArray.java
@@ -0,0 +1,52 @@
+package com.keding.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * @author sunkeding
+ * 旋转数组
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        new Solution().rotate(arr, 6);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    /**
+     * 这个方法基于这个事实：当我们旋转数组 k 次， k%n 个尾部元素会被移动到头部，剩下的元素会被向后移动。
+     * 原始数组                  : 1 2 3 4 5 6 7
+     * 反转所有数字后             : 7 6 5 4 3 2 1
+     * 反转前 k 个数字后          : 5 6 7 4 3 2 1
+     * 反转后 n-k 个数字后        : 5 6 7 1 2 3 4 --> 结果
+     * @param nums
+     * @param start
+     * @param end
+     */
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
